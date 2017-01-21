3ff33dd84e1b14d8ee0875b851cfc95bb1f54a21
James Boullion <jboullion83@gmail.com>
2017-01-21 11:44:43 -0600

    It's Alive!
    The React app has basic functionality. Input into a controlled component
    is updating another element and the parent is controlling the state

--- src-files/scripts/controller.js
+++ src-files/scripts/controller.js
@@ -1,4 +1,5 @@
 
+/*
 function Square (props) {
 
     return (
@@ -115,30 +116,71 @@ class Game extends React.Component {
 		);
 	}
 }
+*/
+
+class InputForm extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<form>
+				<label>Input</label><br />
+				<textarea value={this.props.input} onChange={this.props.handleChange} />
+			</form>
+		);
+	}
+}
+
+class OutputForm extends React.Component {
+	constructor(props) {
+		super(props);
+
+	}
+
+	render() {
+		return (
+			<div>
+				{this.props.output}
+			</div>
+		);
+	}
+}
+
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			input: 'Input',
+			output: 'Output'
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(event) {
+		this.setState({input: event.target.value});
+		this.setState({output: event.target.value});
+	}
+
+	render() {
+		return (
+			<div id="App">
+				<div id="input-form" className="col-6">
+					<InputForm input={this.state.input} handleChange={this.handleChange}/>
+				</div>
+				<div id="input-form" className="col-6">
+					<OutputForm output={this.state.output}/>
+				</div>
+			</div>
+		);
+	}
+}
 
 // ========================================
 
 ReactDOM.render(
-  <Game />,
-  document.getElementById('container')
+	<App />,
+	document.getElementById('container')
 );
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
